440a66cfff960198e1e3cc3d23699a406fcc0a61
peiomi <paydonriley@gmail.com>
2026-05-24 12:27:29 -0500

    Drawing Player

---
+++ README.md
@@ -0,0 +1,26 @@
+# Asteroids
+
+A clone of the classic arcade game built using Python and Pygame. I built this project to better understand vector math and game loops. 
+
+## Prerequisites
+- Python 3.10+
+- Pygame-ce
+
+## Setup
+1. Clone the repository
+2. Install dependencies: `pip install -r requirements.txt`
+3. Run the game: `python3 main.py`
+
+## Controls
+- W/S to move forward/backward
+- A/D to rotate left/right
+- Spacebar to shoot
+
+## Key Features
+- Custom collision detection system using circles.
+- Vector-based movement 
+- Sprite groups for managing game objects
+
+## Credits/Acknowledgments
+
+Built while learning on Boot.dev
--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y= SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
+
         for event in pygame.event.get():
-            pass
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black")
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
